7555e18b8345808ae762c520f75e14927f66dad2
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 10:21:44 +0200

    inserimento metodi new Password,utente

--- PasswordGen/Service/IManagerDb.cs
+++ PasswordGen/Service/IManagerDb.cs
@@ -8,7 +8,10 @@ namespace PasswordGen.Service
         private Context db;
         public IManagerDb(Context db) { this.db = db; }
         abstract public int getId(string username, string password);
-        abstract public int getPassword(int idUtente, string nome);
+        abstract public Password getPassword(int idUtente, string nome);
+
+        abstract public bool newUtente(string username, string password);
+        abstract public bool newPassword(int idUtente, Password password);
 
 
     }
