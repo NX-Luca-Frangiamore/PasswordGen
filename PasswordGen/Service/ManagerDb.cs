14ff8fe239ab2283923adb8d2c8e89827c30f054
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 10:44:11 +0200

    crezione implementazione Imanager

---
+++ PasswordGen/Service/ManagerDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordGen.Model;
+
+namespace PasswordGen.Service
+{
+    public class ManagerDb : IManagerDb
+    {
+        public ManagerDb(Context db) : base(db)
+        {
+        }
+
+        public override int getId(string _username, string _password)
+        {
+            return db.utente.Where(x=>x.username==_username && x.password==_password).Select(x=>x.id).FirstOrDefault();
+        }
+
+        public override Password getPassword(int _idUtente, string _name)
+        {
+            return db.utente.Include(x => x.passwords).Where(x => x.id == _idUtente).Select(x => x.passwords.Where(x => x.name == _name)).FirstOrDefault().FirstOrDefault();
+           
+        }
+
+        public override bool newPassword(int _idUtente, Password _password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool newUtente(string _username, string _password)
+        {
+            if (db.utente.Where(x => x.username == _username) != null) return false;
+            db.utente.Add(new Utente { username = _username, password = _password });
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
