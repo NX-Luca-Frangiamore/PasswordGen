87cc58ee2f1f6ac1d7ecc9cfb8065c16d5eee5f1
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 11:49:17 +0200

    cambio tipo result  getpassword

--- PasswordGen/Service/IManagerDb.cs
+++ PasswordGen/Service/IManagerDb.cs
@@ -8,7 +8,7 @@ namespace PasswordGen.Service
         protected Context db;
         public IManagerDb(Context db) { this.db = db; }
         abstract public int getId(string _username, string _password);
-        abstract public Password getPassword(int _idUtente, string _nome);
+        abstract public Password? getPassword(int _idUtente, string _nome);
 
         abstract public bool newUtente(string _username, string _password);
         abstract public bool newPassword(int _idUtente, Password _password);
