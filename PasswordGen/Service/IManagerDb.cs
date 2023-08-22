0f9ebcc5ffb4478a96dcedab1742ddaf325a2304
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 14:34:57 +0200

    inserimento metoti change Utente/Password

--- PasswordGen/Service/IManagerDb.cs
+++ PasswordGen/Service/IManagerDb.cs
@@ -8,11 +8,19 @@ namespace PasswordGen.Service
         protected Context db;
         public IManagerDb(Context db) { this.db = db; }
         abstract public int getId(string _username, string _password);
-        abstract public Password? getPassword(int _idUtente, string _nome);
-
         abstract public bool newUtente(string _username, string _password);
+        abstract public bool changeUtente(string _username, string _password, string _newUsername, string _newPassword);
+
+        abstract public Password? getPassword(int _idUtente, string _nome);
+        abstract public List<Password>? getPassword(int _idUtente);
+        abstract public bool changePassword(int _idUtente, string _nome,string _newPassword);
         abstract public bool newPassword(int _idUtente, Password _password);
 
+
+        
+        
+        
+     
 
     }
 }
