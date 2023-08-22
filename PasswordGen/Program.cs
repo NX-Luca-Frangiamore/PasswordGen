471ccd9dda191cd7bf9835af9cc0a786244ef951
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 10:06:32 +0200

    set up Program.cs per utilizzo db

--- PasswordGen/Program.cs
+++ PasswordGen/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PasswordGen.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlite<Context>("Data Source=Context.db");//il db si chiama context
+builder.Services.AddDbContext<Context>(options => options.UseLazyLoadingProxies().UseSqlite(builder.Configuration.GetConnectionString("Context")));
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
