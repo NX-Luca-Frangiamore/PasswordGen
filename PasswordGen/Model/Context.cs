b65ff93b2e9d7ecf2d2a738c7e29011d93e26c8f
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 09:58:18 +0200

    creazione context con password non visibili

---
+++ PasswordGen/Model/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PasswordGen.Model
+{
+    public class Context :DbContext
+    {
+        public Context(DbContextOptions opt) : base(opt) { }
+        public DbSet<Utente> utente { get; set; }
+        DbSet<Password> passwords { get; set; }
+
+    }
+}
