7a446052668221790b17d3122274318c9b79d248
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-22 10:20:01 +0200

    creazione intergaccia IManagerDb

---
+++ PasswordGen/Service/IManagerDb.cs
@@ -0,0 +1,15 @@
+﻿
+using PasswordGen.Model;
+
+namespace PasswordGen.Service
+{
+    public abstract class IManagerDb
+    {
+        private Context db;
+        public IManagerDb(Context db) { this.db = db; }
+        abstract public int getId(string username, string password);
+        abstract public int getPassword(int idUtente, string nome);
+
+
+    }
+}
