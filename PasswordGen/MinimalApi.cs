6f4c3ad08d953e64368e8af20132c0cb6259dd6e
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-23 16:20:04 +0200

    inserimento minimalApi get e Post

---
+++ PasswordGen/MinimalApi.cs
@@ -0,0 +1,48 @@
+﻿using PasswordGen.Service;
+using PasswordGen.Model;
+namespace PasswordGen
+{
+    public class MinimalApi
+    {
+        private WebApplication app=null;
+        private static MinimalApi state=null;
+        private MinimalApi(WebApplication app)
+        {
+            this.app = app;
+        }
+        public static MinimalApi getInstant(WebApplication app)
+        {
+            if (state == null)
+                state = new MinimalApi(app);
+            return state;
+        }
+        public void ApiGet()
+        {
+           
+            app.MapGet("/api/get/Password/", async (string username, string password, IManagerDb db) =>
+            {
+                return TypedResults.Ok(db.getPassword(db.getId(username,password)));
+            });
+            app.MapGet("/api/get/Password/{passwordName}", async (string username, string password,string passwordName, IManagerDb db) =>
+            {
+                return TypedResults.Ok(db.getPassword(db.getId(username, password), passwordName));
+            });
+
+
+        }
+        public void ApiPost()
+        {
+            app.MapPost("/api/new/Utente", async (string username, string password, IManagerDb db) =>
+            {
+                return TypedResults.Ok(db.newUtente(username, password));
+            });
+            app.MapPost("/api/new/Password", async (string username, string passwordUtente, string nomePassword, string password, IManagerDb db) =>
+            {
+
+                return TypedResults.Ok(db.newPassword(db.getId(username, passwordUtente), new Password { name = nomePassword, password = password }));
+            });
+        }
+      
+        
+    }
+    }
--- PasswordGen/Program.cs
+++ PasswordGen/Program.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using PasswordGen;
 using PasswordGen.Model;
+using PasswordGen.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlite<Context>("Data Source=Context.db");//il db si chiama context
 builder.Services.AddDbContext<Context>(options => options.UseLazyLoadingProxies().UseSqlite(builder.Configuration.GetConnectionString("Context")));
-
+builder.Services.AddScoped<IManagerDb, ManagerDb>();
 builder.Services.AddControllers();
+
 var app = builder.Build();
+MinimalApi api = MinimalApi.getInstant(app);
 
-app.MapGet("/", () => "Hello World!");
-
+api.ApiPost();
+api.ApiGet();
+api.ApiPut();
 app.Run();
