66e8897ef26a58f9076c3fb388ba9581282dc57a
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-25 09:40:37 +0200

    unit test

---
+++ Test/UnitTest.cs
@@ -0,0 +1,38 @@
+
+using FluentAssertions;
+using PasswordGen.Model;
+
+namespace Test
+{
+    public class UnitTest
+    {
+        [Theory]
+        [InlineData("", "12356")]
+        [InlineData("peppe1", "")]
+        public void Creazione_Utente_NonAvvenuta(string username, string password)
+        {
+            Utente.Create(username, password).Should().BeNull();
+        }
+        [Theory]
+        [InlineData("luca123", "12356","")]
+        [InlineData("peppe1", "333343","fff")]
+        public void Cambio_CredenzialiUtente_NonAvvenuto(string username, string password,string newPassword)
+        {
+
+            Utente u=Utente.Create(username, password);
+            u.ChangeCredenziali("", newPassword);
+           
+            u.UsernameUtente.Should().Be(username);
+            u.PasswordUtente.Should().NotBe(newPassword);
+        }
+        [Theory]
+        [InlineData("gmail", "12")]
+        [InlineData("", "333343")]
+        [InlineData("frfr", "3")]
+        [InlineData("frfr", "ffffffffffffffff3")]
+        public void Creazione_Password_NonAvvenuta(string name, string password)
+        {
+            PasswordModel.Create(name, password).Should().BeNull();
+        }
+    }
+}
